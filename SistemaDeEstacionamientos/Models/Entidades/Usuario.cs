da345e5a0caaabcbdd5e131475647eccb0a6855c
lazoescobar <agustin_lazo_e@hotmail.com>
2020-07-16 16:42:25 -0400

    se creo al carpeta entidades con las clases igualadas a las tablas en las base de datos

---
+++ SistemaDeEstacionamientos/Models/Entidades/Usuario.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SistemaDeEstacionamientos.Models.Entidades
+{
+    public class Usuario
+    {
+        private int IdUsuario;
+        private string Nombre;
+        private string NombreUsuario;
+        private string Contrasenia;
+
+        public int idUsuario
+        {
+            get { return this.IdUsuario; }
+
+            set { this.IdUsuario = value;  }
+        }
+
+        public string nombre
+        {
+            get { return this.Nombre;  }
+
+            set { this.Nombre = value;  }
+        }
+
+        public string nombreUsuario
+        {
+            get { return this.NombreUsuario; }
+
+            set { this.NombreUsuario = value; }
+        }
+
+        public string contrasenia
+        {
+            get { return this.Contrasenia; }
+
+            set { this.Contrasenia = value;  }
+        }
+    }
+}
---
+++ SistemaDeEstacionamientos/Models/Entidades/Vehiculo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SistemaDeEstacionamientos.Models.Entidades
+{
+    public class Vehiculo
+    {
+    }
+}
