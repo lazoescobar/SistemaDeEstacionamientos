4f70212ef3e64ec98e5dc0dd0f9abb14affdedad
lazoescobar <agustin_lazo_e@hotmail.com>
2020-07-16 17:09:44 -0400

    se trabajo en esta clase

--- SistemaDeEstacionamientos/Models/Entidades/Aparcamiento.cs
+++ SistemaDeEstacionamientos/Models/Entidades/Aparcamiento.cs
@@ -7,5 +7,19 @@ namespace SistemaDeEstacionamientos.Models.Entidades
 {
     public class Aparcamiento
     {
+
+        private int Id_Aparcamiento;
+        private DateTime FechaEntrada;
+        private DateTime HoraEntrada;
+        private DateTime FechaSalida;
+        private DateTime HoraSalida;
+        private Vehiculo vehicu;
+
+        public Aparcamiento(Vehiculo vehiculo) {
+
+            this.vehicu = vehiculo;
+        }
+
+        
     }
 }
