472d8a604430088cb0a25f37d0ec9e8b392f6469
lazoescobar <agustin_lazo_e@hotmail.com>
2020-07-16 16:50:20 -0400

    se crearon los metodos get y set

--- SistemaDeEstacionamientos/Models/Entidades/Vehiculo.cs
+++ SistemaDeEstacionamientos/Models/Entidades/Vehiculo.cs
@@ -7,5 +7,29 @@ namespace SistemaDeEstacionamientos.Models.Entidades
 {
     public class Vehiculo
     {
+        private string Patente;
+        private string Modelo;
+        private string Marca;
+
+        public string patente
+        {
+            get { return this.Patente; }
+
+            set { this.Patente = value; }
+        }
+
+        public string modelo
+        {
+            get { return this.Modelo; }
+
+            set { this.Modelo = value; }
+        }
+
+        public string marca
+        {
+            get { return this.Marca; }
+
+            set { this.Marca = value; }
+        }
     }
 }
